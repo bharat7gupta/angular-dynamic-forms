06ec830d667d61b4a7bbdc6f7bc2a30670f36269
Bharat Gupta <bharat7gupta@gmail.com>
2017-04-29 22:45:34 +0530

    dynamic form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,18 @@
-import { Component } from '@angular/core';
-
+import { Component }       from '@angular/core';
+import { QuestionService } from './question.service';
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+    <div>
+      <h2>Job Application for Heroes</h2>
+      <dynamic-form [questions]="questions"></dynamic-form>
+    </div>
+  `,
+  providers:  [QuestionService]
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent {
+  questions: any[];
+  constructor(service: QuestionService) {
+    this.questions = service.getQuestions();
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent }  from './app.component';
+import { DynamicFormComponent }         from './dynamic-form.component';
+import { DynamicFormQuestionComponent } from './dynamic-form-question.component';
 
 @NgModule({
-  imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  imports:      [ BrowserModule, ReactiveFormsModule ],
+  declarations: [ AppComponent, DynamicFormComponent, DynamicFormQuestionComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
